955b074a3eabd0182f2f486bc4f2ce15f3c7ac9d
daxas-boop <daxas.sdw@hotmail.com>
2020-09-29 08:56:26 -0300

    Changes from material.ui to tailwind.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import {Icon} from '@material-ui/core'
-import Input from '@material-ui/core/Input';
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
-import List from './components/Overview'
+import List from './components/Overview';
+import uniqid from "uniqid";
 
 class App extends Component {
   constructor(props) {
@@ -12,29 +8,27 @@ class App extends Component {
 
     this.state = {
       tasks: [{
-        todo:'Dummy Task',
-        id: 1
-      }],
+        task:'Dummy Task',
+        id: uniqid()
+      }
+      ],
       input: '',
     }
 
-    this.handleClick = this.handleClick.bind(this);
+    this.addTask = this.addTask.bind(this);
     this.removeTask = this.removeTask.bind(this);
   }
 
-  handleClick () {
+  addTask () {
     const newTask = {
-      todo:this.state.input,
-      id: this.state.tasks.length + 1
+      task: this.state.input,
+      id: uniqid()
     }
-
     let joined = this.state.tasks.concat(newTask);
     this.setState({ tasks: joined, input: ''})
   }
 
-  removeTask(e) {
-    const id = e.target.tabIndex
-    console.log(id);
+  removeTask(id) {
     this.setState({tasks: [...this.state.tasks.filter( task => 
       task.id !== id
    )]})
@@ -42,41 +36,26 @@ class App extends Component {
 
   render() {
     return(
-      <Container maxWidth="md">
-        <Box display="flex" justifyContent="center">
-          <ul>
-            {this.state.tasks.map( (task) => (
-              <Box key={'box'+ task.id} display="flex" justifyContent="center" alignItems="center" bgcolor='info.main' p={1}>
-                <List tabIndex={task.id} key={'list'+task.id} task={task.todo} id={task.id} ></List>
-                <button 
-                  tabIndex={task.id}
-                  onClick= { this.removeTask }
-                >
-                  Delete
-                </button>
-              </Box>
-            ))}
-          </ul>
-        </Box>
-        <Box display="flex" justifyContent="center">
-          <Input 
-            placeholder="Task to do" 
-            value={this.state.input}
-            onChange={(e) => this.setState({ input: e.target.value })} 
+      <div>
+        <List tasks={this.state.tasks} removeTask={this.removeTask} ></List>
+        <form 
+          onSubmit={(e)=> {e.preventDefault()}}
+          className= 'bg-white shadow-md flex justify-center mt-5 pb-5 pt-5' 
+        >
+          <input
+            value= {this.state.input}
+            onChange={(e) => this.setState({ input: e.target.value })}
+            placeholder="Task to do"
+            className='bg-teal-100 border-2 border-teal-200 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-teal-500'
           />
-          <Button 
-            variant="contained" 
-            color="primary" 
-            endIcon={<Icon>send</Icon>}
-            onClick={ this.handleClick }
-          >
-            Send
-          </Button>
-        </Box>
-      </Container>
+          <button 
+            onClick={ this.addTask }
+            className= 'bg-teal-600 hover:bg-teal-800 text-white font-bold py-2 px-4 rounded ml-2'
+          >Send</button>
+        </form>
+      </div>
     )
   }
-
 }
 
 export default App;
