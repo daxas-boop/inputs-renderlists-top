bc35336ba1de51177be7b53ce2bea19e8c1f4fb6
daxas-boop <daxas.sdw@hotmail.com>
2020-09-29 10:50:13 -0300

    Adds edit button and props to make it work.

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react'
-import uniqid from "uniqid";
 
 class List extends Component {
 
     render() {
         return(
-        <ol className= 'max-w-screen-sm m-auto mt-2'>
+        <ol className= 'text-sm xl:max-w-screen-xl xl:text-xl  lg:max-w-screen-lg lg:text-lg md:max-w-screen-md md:text-base sm:max-w-screen-sm sm:text-sm m-auto mt-2'>
             {this.props.tasks.map( (task) => (
-                <div className= 'flex justify-center bg-teal-300 pl-3 pr-3' key={'box'+ task.id} >
-                    <li key={uniqid()} id={uniqid()} className='p-4 mr-5 font-semibold text-lg'>{task.task}</li>
+                <div className= 'flex fluid justify-center bg-teal-300 pl-3 pr-3 ' key={'box'+ task.id}>
+                    <input 
+                        key={task.id} 
+                        id={task.id} 
+                        defaultValue={task.task} 
+                        disabled={task.disabledInput}
+                        onChange={ (e) => {this.props.handleInputChange(e.target.value, task)} }
+                        className={task.disabledInput ? 'pt-2 pb-2 bg-teal-300 font-semibold text-center rounded self-center' :
+                        'pt-3 pb-3 mr-5 bg-red-300 font-semibold text-center rounded self-center md:ml-2' } 
+                        />
+                    <button
+                        onClick= { () => {this.props.editTask(task)} }
+                        className= 'btn bg-green-500 hover:bg-green-700 text-white font-bold py-2 mr-1 px-3 rounded self-center'
+                    >
+                       {task.disabledInput ? 'Edit' : 'End edit'} 
+                    </button>
                     <button
                         onClick= { () =>  {this.props.removeTask(task.id)} }
-                        className= 'btn bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded ml-2 self-center'
+                        className= 'btn bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-1 rounded self-center'
                     >
                         Delete
                     </button>
