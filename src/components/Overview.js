a14ce0e35965583f522f1aa0a1fe766b34b21ac8
daxas-boop <daxas.sdw@hotmail.com>
2020-09-29 08:56:46 -0300

    Adds the whole list to the overview component

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react'
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import uniqid from "uniqid";
 
 class List extends Component {
+
     render() {
         return(
-            <ListItem>
-                <ListItemText
-                    primary={this.props.task}
-                />
-            </ListItem>
-        )
-    }
+        <ol className= 'max-w-screen-sm m-auto mt-2'>
+            {this.props.tasks.map( (task) => (
+                <div className= 'flex justify-center bg-teal-300 pl-3 pr-3' key={'box'+ task.id} >
+                    <li key={uniqid()} id={uniqid()} className='p-4 mr-5 font-semibold text-lg'>{task.task}</li>
+                    <button
+                        onClick= { () =>  {this.props.removeTask(task.id)} }
+                        className= 'btn bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded ml-2 self-center'
+                    >
+                        Delete
+                    </button>
+                </div>
+            ))}
+        </ol>
+    )}
 }
 
 export default List;
